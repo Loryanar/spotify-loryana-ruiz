e8efb7aec0d7842b243c4fca0ee9dcc8d97b8328
Loryanar <47923247+Loryanar@users.noreply.github.com>
2021-07-14 01:29:24 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@
 
 
  app.use(require('./routes/routes'));
-
+ puerto = process.env.PORT || 9000;
  app.use(require('./routes/rout'));
  app.use(require('./routes/routp'));
  app.use(require('./routes/routesa'));
- app.listen(9000,function(){
+ app.listen(puerto,function(){
      console.log("Server run..");
  })
  
