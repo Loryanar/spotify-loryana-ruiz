3e948ffdcff71d5ebc2cf7188c922e511d30616c
Loryanar <47923247+Loryanar@users.noreply.github.com>
2021-12-14 09:37:07 -0400

    Add files via upload

--- helpers/db.js
+++ helpers/db.js
@@ -6,5 +6,12 @@ const bd = new Pool({
   database: "d1v9p2pjfqackr",
   password: "5381beb09aa09611d83c0eaaf118a15fc3eb3474de86b393de28f9327a4016e7",
   port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  }
 });
 module.exports = bd;
