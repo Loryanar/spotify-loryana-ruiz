74a0fd1de165b331690902a0a3b52c4a6742bcf0
Loryanar <47923247+Loryanar@users.noreply.github.com>
2021-12-14 12:29:29 -0400

    Add files via upload

--- helpers/db.js
+++ helpers/db.js
@@ -4,13 +4,13 @@ const bd = new Pool({
   database: process.env.POSTGRES_DB,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  host: "ec2-44-194-225-27.compute-1.amazonaws.com",
+  host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   ssl:true,
   dialectOptions: {
     ssl: {
       require: true, // This will help you. But you will see nwe error
-      rejectUnauthorized: false // This line will fix new error
+      
     }
   }
 });
