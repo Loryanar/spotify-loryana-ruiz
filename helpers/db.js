025c15c01452d483b2fabbe7402cc5eebe8749dd
Loryanar <47923247+Loryanar@users.noreply.github.com>
2021-12-14 15:21:02 -0400

    Add files via upload

--- helpers/db.js
+++ helpers/db.js
@@ -1,12 +1,11 @@
 const { Pool } = require("pg")
 
 const bd = new Pool({
-  user: "camnjfokidbfwu",
-  host: "ec2-44-194-225-27.compute-1.amazonaws.com",
-  database: "d1v9p2pjfqackr",
-  password: "5381beb09aa09611d83c0eaaf118a15fc3eb3474de86b393de28f9327a4016e7",
-  port: 5432,
-  dialect: "postgres",
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   ssl:true,
   dialectOptions: {
     ssl: {
