7a266edf4bd42af7868c2a6763c6ac4e7023d970
Loryanar <47923247+Loryanar@users.noreply.github.com>
2021-12-13 22:16:39 -0400

    Add files via upload

--- helpers/db.js
+++ helpers/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg")
 
 const bd = new Pool({
-  user: "lpvogxhmptuoyh",
-  host: "ec2-34-193-101-0.compute-1.amazonaws.com",
-  database: "db0mfeotitqr13",
-  password: "0ca0d37e9bbaddeaa9deaa5b8e4d1f90f7c077c61d9830b88e706384ab7d4326",
+  user: "camnjfokidbfwu",
+  host: "ec2-44-194-225-27.compute-1.amazonaws.com",
+  database: "d1v9p2pjfqackr",
+  password: "5381beb09aa09611d83c0eaaf118a15fc3eb3474de86b393de28f9327a4016e7",
   port: 5432,
 });
 module.exports = bd;
